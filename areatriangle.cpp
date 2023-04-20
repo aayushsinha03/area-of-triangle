75c6ca19cdcc973dcfbd3291a1a6b3c278edf1ed
Aayush sinha <130181905+aayushsinha03@users.noreply.github.com>
2023-04-21 02:15:40 +0530

    Add files via upload

---
+++ areatriangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b;
+    int h;
+    float a;
+    cout<<"enter the base"<<endl;
+    cin>>b;
+    cout<<"enter the height"<<endl;
+    cin>>h;
+    a=b*h/2;
+        cout<<"area of triangle is "<< a <<endl;
+        return 0;
+
+
+}
